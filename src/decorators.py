fd515303c565e728d86955e5a9c8358abd34d860
adibozzhanov <bozzhanov.at@gmail.com>
2021-06-20 17:04:37 +0100

    fix: formatting of decorators

--- src/decorators.py
+++ src/decorators.py
@@ -8,7 +8,6 @@ def static_command(func):
     def command(update, context):
         chat_id = update.message.chat.id
         text = func()
-        print(bot)
         bot.send_message(chat_id = chat_id, text = text)
     return command
 
@@ -18,9 +17,7 @@ def named_static_command(func):
         chat_id = update.message.chat.id
         name = update.message.from_user.first_name
         text = func(name)
-
         bot.send_message(chat_id = chat_id, text = text)
-
     return command
         
         
